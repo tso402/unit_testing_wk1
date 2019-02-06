e673f216ebd506ae34e5953c0d50d13f10c0a865
James Ault <james.ault@johnlewis.co.uk>
2019-02-06 11:15:37 +0000

    completed the example unit test

--- spec/unit_test_spec.rb
+++ spec/unit_test_spec.rb
@@ -1,4 +1,10 @@
 require './lib/unit_test.rb'
 
 describe FileSystem do
+  it 'can add items to storage' do
+    file_system = FileSystem.new
+    file = 'James'
+    file_system.store(file)
+    expect(file_system.display_files).to include(file)
+  end
 end
